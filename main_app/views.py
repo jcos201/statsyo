75add57f2e6eaec72d633a818dbeff9b15d1f8f4
Stuart Palmer <palmersf@gmail.com>
2021-02-27 11:07:00 -0500

    creates initial shell app

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def stats(request):
+    return render(request, 'stats.html')
