b604f4ad0ad894ddcb760c4440c83044374ff867
Jorge L. Cossi <jcos201@gmail.com>
2021-02-27 15:57:07 -0500

    Adds SECRET.env and Models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from django.urls import reverse
 
 # Create your models here.
+
+class Fav_Team(models.Model):
+    team_id = models.CharField(max_length=100)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Fav_Player(models.Model):
+    player_id = models.CharField(max_length=100)
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Fav_List(models.Model):
+    name = models.CharField(max_length=100)
+
+    fav_players = models.ManyToManyField(Fav_Player)
--- statsyo/settings.py
+++ statsyo/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gb0^a2#9k0skw^d)@%ug8+f$3_x=pk&y)wa!n#17hl9=+em^s0'
+SECRET_KEY = os.getenv('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
